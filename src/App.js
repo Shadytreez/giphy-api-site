deef0d71f51ae6e391da7f66a9ee17a54c7fabe6
Truman <trumanngny@yahoo.com>
2021-06-11 22:09:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import City from "./city"
 import './App.css';
+class App extends Component{
+  
+    
+    state={
+      cities:[],
+      v: [],
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  getTrending = () =>
+  { 
+    const url = "http://api.giphy.com/v1/gifs/trending?api_key=eBeXFUz7YRUQL4jA0j53KCQ0t12q8y6R";
+
+    if(zipCode.length === 5)
+    {
+      fetch(url).
+      then(response => response.json()).
+      then(responseJson => {
+        console.log(responseJson);
+        this.setState({cities: responseJson});
+      }).
+      then(city => {
+        for(let i in this.state.cities)
+        {
+          console.log(i);
+          var joined = this.state.v.concat(<City city={this.state.cities[i]}/>);
+          console.log(joined);
+          this.setState({ v: joined });
+          console.log(this.state.v[i]);
+          console.log(this.state.cities[i]);
+        } 
+      }).catch((error) => {
+        alert("not a valid zip code");
+      });
+    }else{
+      alert("zip code should be 5 numbers");
+    }
+   
+  }
+
+  getRandom(){
+    const url = "http://api.giphy.com/v1/gifs/random?api_key=eBeXFUz7YRUQL4jA0j53KCQ0t12q8y6R";
+  }
+
+  getSearch(){
+    const searchTerm = document.getElementById("search").value;
+    const url = "http://api.giphy.com/v1/gifs/search?q=" +SEARCH+TERM+GOES+HERE+"&api_key=eBeXFUz7YRUQL4jA0j53KCQ0t12q8y6R";
+  }
+  
+
+  
+
+  render(){
+   
+
+    return (
+    <div className="content">
+      <h1>Zip Code Search</h1>
+      <form id ="form">
+         <p>Zip Code </p> <input id="search" placeholder="12345" type="text"/>
+         <button type ="button" onClick={this.getCities}> Submit</button>
+       </form>
+        {this.state.v}
+       
+    </div>)
+  }
 }
-
 export default App;
